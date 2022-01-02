40ff9928a3dcf28d7e4b9aa885e8e714d3de8cb5
Margarita Kachalko <margarita.kachalko@gmail.com>
2022-01-02 16:14:22 +0300

    Fix: email and name validation

--- src/modules/forms.js
+++ src/modules/forms.js
@@ -5,6 +5,11 @@ const forms = () => {
   const phone = document.querySelectorAll("[name=user_phone]");
   const message = document.querySelectorAll("[name=user_message]");
 
+  name.forEach((item) =>
+    item.addEventListener("input", (e) => {
+      e.target.value = e.target.value.replace(/[^а-я\s\-]/gi, "");
+    })
+  );
   message.forEach((item) =>
     item.addEventListener("input", (e) => {
       e.target.value = e.target.value.replace(/[^а-я\s\-]/gi, "");
@@ -18,6 +23,21 @@ const forms = () => {
   phone.forEach((item) =>
     item.addEventListener("input", (e) => {
       e.target.value = e.target.value.replace(/[^\d\(\)\-]/g, "");
+    })
+  );
+  forms.forEach((item) =>
+    item.addEventListener("submit", (e) => {
+      const email = item.querySelector("[name=user_email]");
+
+      e.preventDefault();
+
+      if (email.value.match(/[\w\-\_\!\~\*\'\.]+@([\w]+\.)+[\w]+/gi)) {
+        email.style.backgroundColor = "";
+        console.log("форма отправлена");
+      } else {
+        email.style.backgroundColor = "#edadc0";
+        console.log("неверная форма email");
+      }
     })
   );
 };
